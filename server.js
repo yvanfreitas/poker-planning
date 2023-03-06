b03e4f04698cd2b1c9a7b3006dc4266821dd0fad
yvanfreitas <yvan.freitas@gmail.com>
2023-03-06 01:41:57 -0300

    adding env file and bin command

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 let games = [];
 let players = [];
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
 
 io.on('connection', (socket) => {
