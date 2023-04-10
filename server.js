c45c7a547e47f6ecda0f1b13074797cf881dfd54
yvanfreitas <yvan.freitas@gmail.com>
2023-04-10 19:39:26 -0300

    refactor by chatgpt

--- server.js
+++ server.js
@@ -5,42 +5,107 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+class WebSocketServer {
+  constructor(port) {
+    this.port = port;
+    this.app = express();
+    this.server = http.createServer(this.app);
+    this.io = new Server(this.server);
+    this.games = new Map();
+
+    this.server.listen(this.port, () => {
+      console.log(`listening on *:${this.port}`);
+    });
+
+    this.io.on('connection', (socket) => this.handleConnection(socket));
+  }
+
+  handleConnection(socket) {
+    socket.on('game-input', (payload) => {
+      console.log('---- event received ----');
+      console.log(payload);
+
+      const gameId = payload.data.gameId != undefined ? payload.data.gameId : payload.gameId;
+      if (!gameId) return;
+
+      let game = this.games.get(gameId);
+
+      if (!game) {
+        game = new Game(gameId);
+        this.games.set(gameId, game);
+      }
+
+      if (!game.hasPlayer(socket.id)) {
+        game.registerPlayer(socket.id);
+      }
+
+      game.broadcastEventToPlayers(payload);
+    });
+
+    socket.on('reconnect', () => {
+      this.handleReconnection(socket);
+    });
+  }
+
+  handleReconnection(socket) {
+    const gameId = this.players.get(socket.id);
+    if (!gameId) return;
+
+    const game = this.games.get(gameId);
+    if (!game) return;
+
+    game.sendUnsentEvents(socket.id);
+  }
+}
+
+class Game {
+  constructor(id) {
+    this.id = id;
+    this.players = new Map();
+    this.events = [];
+  }
+
+  hasPlayer(socketId) {
+    return this.players.has(socketId);
+  }
+
+  registerPlayer(socketId) {
+    this.players.set(socketId, true);
+  }
+
+  addEvent(payload) {
+    const event = {
+      payload,
+      sentToPlayers: new Map(),
+    };
+
+    this.events.push(event);
+    return event;
+  }
+
+  broadcastEventToPlayers(payload) {
+    const event = this.addEvent(payload);
+
+    this.players.forEach((_value, socketId) => {
+      event.sentToPlayers.set(socketId, true);
+
+      console.log(`------ event emited ${socketId} ------`);
+      console.log(payload);
+
+      webSocketServer.io.sockets.in(socketId).emit('game-input', payload);
+    });
+  }
+
+  sendUnsentEvents(socketId) {
+    this.events.forEach((event) => {
+      if (!event.sentToPlayers.get(socketId)) {
+        console.log(`event emited ${socketId} ${JSON.stringify(event.payload)}`);
+        webSocketServer.io.sockets.in(socketId).emit('game-input', event.payload);
+        event.sentToPlayers.set(socketId, true);
+      }
+    });
+  }
+}
+
 const port = process.env.PORT || 3000;
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-let games = [];
-let players = [];
-
-server.listen(port, () => {
-  console.log(`listening on *:${port}`);
-});
-
-io.on('connection', (socket) => {
-  console.log(`socket connected ${socket.id}`);
-
-  socket.on('game-input', (payload) => {
-    if (players[socket.id] == undefined) {
-      registerPlayer(socket.id, payload.data?.gameId);
-    }
-    const gameId = players[socket.id];
-    broadcastEventToPlayers(gameId, payload);
-  });
-});
-
-async function broadcastEventToPlayers(gameId, payload) {
-  games[gameId]?.forEach((socketId) => {
-    emit(socketId, payload);
-  });
-}
-
-async function emit(socketId, data) {
-  console.log(`event emited ${socketId} ${data}`);
-  io.sockets.in(socketId).emit('game-input', data);
-}
-
-function registerPlayer(socketId, gameId) {
-  if (!games[gameId]) games[gameId] = [];
-  games[gameId].push(socketId);
-  players[socketId] = gameId;
-}
+const webSocketServer = new WebSocketServer(port);
